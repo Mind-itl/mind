8b8a0985aee5b1cf1219e9cd55b67c43456e6145
Semenov Roman <3908441@gmail.com>
2018-09-15 13:19:01 +0300

    added registration for students

--- controls/signup.php
+++ controls/signup.php
@@ -1,5 +1,11 @@
 <?php
+	require_once 'passwords.php';
+
 	class Signup_control extends Control {
+		private static $post_fields = [
+			"login", "password", "sname", "name", "fname", "bday"
+		];
+
 		public function __construct() {
 			parent::__construct("signup");
 		}
@@ -9,6 +15,10 @@
 		}
 
 		protected function get_data(array $args): array {
+			if (isset_post_fields(...static::$post_fields)) {
+				$this->post();
+			}
+
 			return [
 				"CLASSES" => $this->get_classes()
 			];
@@ -23,6 +33,46 @@
 			}
 
 			return json_encode($r);
+		}
+
+		private function post() {
+			$user = $_POST;
+
+			$login = $user["login"];
+			$password = $user["password"];
+			$role = $user["role"];
+
+			$name = $user["name"];
+			$fname = $user["fname"];
+			$sname = $user["sname"];
+
+			$bday = $user["bday"];
+			$class = $user["class"] ?? "";
+
+			list($class_num, $class_lit) = explode('-', $class);
+
+			register_user($user["login"], $user["password"], $user["role"]);
+			if ($user["role"] == "teacher") {
+				sql_query("
+					INSERT INTO teachers (
+						GIVEN_NAME, FAMILY_NAME, FATHER_NAME, ROLE, LOGIN, BIRTHDAY
+					) VALUES (
+						'$name', '$sname', '$fname', '', '$login', '$bday'
+					)
+				");
+			} else {
+				sql_query("
+					INSERT INTO students (
+						GIVEN_NAME, FAMILY_NAME, FATHER_NAME, LOGIN, CLASS_NUM, CLASS_LIT, BIRTHDAY
+					) VALUES (
+						'$name', '$sname', '$fname', '$login', $class_num, $class_lit, '$bday'
+					)
+				");
+			}
+
+			enter_user($login, $password);
+
+			redirect("/");
 		}
 	}
 ?>	
