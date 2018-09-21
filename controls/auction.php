620701aaba8e374b86cf164d0e6eeeaee6a9e311
Semenov Roman <3908441@gmail.com>
2018-09-21 16:47:27 +0300

    some velosiped and kostil

--- controls/auction.php
+++ controls/auction.php
@@ -10,9 +10,21 @@
 			return [
 				"points" => [
 					"count" => get_curr()->get_points(),
-					"noun" => get_points_case(get_curr()->get_points())
-				]
+					"noun" => get_points_case(get_curr()->get_points()),
+				],
+				"scripts" => $this->scripts()
 			];
+		}
+
+		protected function scripts(): string {
+			$str = "";
+			$dir = ROOT."public_html/js/auction/";
+
+			foreach (scandir($dir) as $file) {
+				$str .= file_get_contents($dir.$file);
+			}
+
+			return $str;
 		}
 	}
 ?>
