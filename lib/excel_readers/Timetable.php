d0fda01bc35e2dbdd840524b21aaa2d040d93bae
Semenov Roman <3908441@gmail.com>
2018-09-22 10:54:44 +0300

    almost done

--- lib/excel_readers/Timetable.php
+++ lib/excel_readers/Timetable.php
@@ -1,7 +1,28 @@
 <?php
 	class Timetable extends Excel_reader {
+		static function process(Closure $f): array {
+			//todo
+		}
+
 		static function handle(Closure $f) {
-			
+			foreach (static::process($f) as $lesson) {
+				static::add_lesson($lesson);
+			}
 		};
+
+		static function add_lesson($lesson) {
+			safe_query("
+				INSERT INTO LESSONS (
+					CLASS, WEEKDAY, NUMBER, LESSON, PLACE
+				) VALUES (
+					?s, ?s, ?i, ?s, ?s
+				)",
+					$lesson["class"],
+					$lesson["day"],
+					$lesson["num"],
+					$lesson["name"],
+					$lesson["place"]
+			)
+		}
 	}
 ?>
