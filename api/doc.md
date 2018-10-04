b62e2ac373d41adc6729c01fcb170fa159e90492
Semenov Roman <3908441@gmail.com>
2018-10-04 18:00:19 +0300

    api

---
+++ api/doc.md
@@ -0,0 +1,48 @@
+# Objects
+
+
+## Login
+`string`
+
+## User
+```
+{
+	"login": Login,
+	"names": {
+		"given": string, "father": string, "family": string
+	},
+	"is_student": bool,
+	"roles": Role[] | null,
+	"role_args": string[Role] | null,
+	"points": int | null,
+}
+```
+
+## Role
+```
+enum {
+	"student", "teacher", "diric", "classruk", "vospit", "zam", "admin", "predmet"
+}
+```
+
+## Cause
+`string`
+
+String code that determines the cause of adding points, for example `C1`, `A2`
+
+## Transaction
+```
+{
+	"time": string,
+	"from_login": Login,
+	"to_login": Login,
+	"points": int,
+	"cause": Cause
+}
+```
+
+# Methods
+
+## getUser
+**arguments**: `login: Login`,
+**return**: `User`
--- api/getUser.php
+++ api/getUser.php
@@ -20,9 +20,29 @@
 
 		$user = get_user($_GET['login']);
 
-		return [
+		$ret = [
+			"login" => $user->get_login(),
 			"is_student" => $user->is_student(),
-			"name" => $user->get_full_name()
+			"names" => [
+				"given" => $user->get_given_name(),
+				"family" => $user->get_family_name(),
+				"father" => $user->get_father_name(),
+			],
 		];
+
+		if ($user->is_teacher()) {
+			$ret["roles"] = $user->get_roles();
+			$ret["roles_args"] = [];
+
+			foreach ($user->get_roles() as $role) {
+				$role_arg = $user->get_role_arg($role);
+				if (isset($role_arg))
+					$ret["role_args"][$role] = $role_arg;
+			}
+		} else {
+			$ret["points"] = $user->get_points();
+		}
+
+		return $ret;
 	}
 ?>
