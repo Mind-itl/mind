1d4aeb47f97b5775196b364b127c8de0984f32d8
Semenov Roman <3908441@gmail.com>
2018-10-08 14:38:26 +0300

    Vospits at internat page

--- controls/internat.php
+++ controls/internat.php
@@ -10,7 +10,25 @@
 			if (isset_post_fields("song-singer", "song-name"))
 				add_music($_POST["song-singer"], $_POST["song-name"], get_curr());	
 
-			return [];
+			return [
+				"vospits" => $this->get_vospits()
+			];
+		}
+
+		public function get_vospits(): array {
+			$today = (new DateTime())->format('l');
+
+			$r = safe_query("SELECT * FROM dutes WHERE DAY=?s", $today);
+
+			$ret = [];
+			foreach ($r as $v) {
+				$ret[] = [
+					"login" => $v["LOGIN"],
+					"block" => $v["BLOCK"]
+				];
+			}
+
+			return $ret;
 		}
 	}
 ?>
