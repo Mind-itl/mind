6f14f85e2eabc2cca3efdc274653b4c85fa5f88e
Haziev Danis <danishaz112345@gmail.com>
2018-10-12 19:45:17 +0300

    added timetable reading from excel

--- lib/excel_readers/Timetable.php
+++ lib/excel_readers/Timetable.php
@@ -11,7 +11,25 @@
 		}
 
 		static function process(Closure $f): array {
-			//todo
+			$lessons = [];
+			$c = $f(0, 0);
+			$mx = 30;
+			for ($i = 1; $i <= $mx; $i += 3) {
+				$denek = $f($i, 0);
+				if ($denek == "")
+					break;
+				for ($j = 1; $j <= $mx; $j++) {
+					$arr = [];
+					$arr["class"] = $c;
+					$arr["day"] = $denek;
+					$arr["num"] = $j;
+					$arr["name"] = $f($i, $j);
+					$arr["place"] = $f($i + 2, $j);
+					$arr["teacher"] = $f($i + 1, $j);
+					$lessons[] = $arr;
+ 				}
+			}
+			return $lessons;
 		}
 
 		static function add_lesson($lesson) {
