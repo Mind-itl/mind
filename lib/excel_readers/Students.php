43254a623b148773392da40ada5a9ed53ec3972f
Semenov Roman <3908441@gmail.com>
2018-10-22 12:52:33 +0300

    students reading

--- lib/excel_readers/Students.php
+++ lib/excel_readers/Students.php
@@ -1,4 +1,6 @@
 <?php
+	require_once LIBS."passwords.php";
+
 	class Students_excel_reader extends Excel_reader {
 		static function get_name(): string {
 			return "Ученики";
@@ -19,18 +21,22 @@
 			$n = 4; // количество циферок в логине
 			$i = 1;
 			$arr = [];
-			while ($f(1, $i) != "") {
+			while ($f(0, $i) != "") {
 				$arr[$i] = [];
-				$arr[$i]["name"] = self::format_name($f(1, $i));
-				$arr[$i]["father_name"] = self::format_name($f(2, $i));
-				$arr[$i]["family_name"] = self::format_name($f(3, $i));
-				$arr[$i]["num"] = $f(4, $i);
-				$arr[$i]["lit"] = $f(5, $i);
-				$arr[$i]["birthday"] = $f(6, $i);
+				$arr[$i]["family_name"] = $f(1, $i);
+				$arr[$i]["name"] = $f(2, $i);
+				$arr[$i]["father_name"] = $f(3, $i);
+
+				$class = $f(4, $i);
+				list($aaa, $bbb) = explode('-', $class);
+				$arr[$i]["num"] = $aaa;
+				$arr[$i]["lit"] = $bbb;
+
+				//$arr[$i]["birthday"] = $f(6, $i);
 				while (true) {
 					$str = "";
 					for ($j = 0; $j < $n; $j++) {
-						$rand = rand_int(0, 9);
+						$rand = random_int(0, 9);
 						$str .= "$rand";
 					}
 					$a = safe_query_assoc("
@@ -49,29 +55,35 @@
 		}
 
 		static function add_student($student) {
-			$arr = self::process($get);
-			foreach ($arr as $i) {
-				$a = safe_query("
-					SELECT COUNT(LOGIN) AS COUNT FROM students 
-					WHERE
-					 	LOGIN = ?s
-				", $i["login"]
-				)["COUNT"];
-				if (intval($a) != 0)
-					continue;
-				$pas = "";
-				for ($j = 0; $j < 5; $j++) {
-					$pas .= (rand_int(0, 9));
-				}
-				safe_query("
-					INSERT INTO students (
-						GIVEN_NAME, FATHER_NAME, FAMILY_NAME, CLASS_NUM, CLASS_LIT, BIRTHDAY, LOGIN
-					) VALUES (
-						?s, ?s, ?s, ?i, ?s, ?s, ?s
-					)", $i["name"], $i["father_name"], $i["family_name"], $i["num"], $i["lit"], $i["birthday"], $i["login"]
-				);
-				register_user($i["login"], $pas, "student");
-				// return login and password to excel
+			$i = $student;
+			$a = safe_query_assoc("
+				SELECT COUNT(LOGIN) AS COUNT FROM students 
+				WHERE
+					LOGIN = ?s
+			", $i["login"]
+			)["COUNT"];
+			if (intval($a) != 0)
+				return;
+
+			$pas = "";
+			for ($j = 0; $j < 5; $j++) {
+				$pas .= (random_int(0, 9));
+			}
+
+			safe_query("
+				INSERT INTO students (
+					GIVEN_NAME, FATHER_NAME, FAMILY_NAME, CLASS_NUM, CLASS_LIT, LOGIN
+				) VALUES (
+					?s, ?s, ?s, ?i, ?s, ?s
+				)", $i["name"], $i["father_name"], $i["family_name"], intval($i["num"]), $i["lit"], $i["login"]
+			);
+			register_user($i["login"], $pas, "student");
+
+			$user = get_user($i["login"]);
+			$teacher = $user->get_classruk();
+
+			if ($teacher) {
+				add_notification($teacher, $user, "Пользователь зарегестрировался. Логин: ".$i["login"]." Пароль: $pas");
 			}
 		}
 	}
