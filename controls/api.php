050254ee2280758679c375647c6a686da810f728
Semenov Roman <3908441@gmail.com>
2018-10-25 15:08:26 +0300

    add checking token in api (vrode no na samom dele net)

--- controls/api.php
+++ controls/api.php
@@ -4,6 +4,17 @@
 	class Api_control extends Control {
 		public function __construct(string $name) {}
 
+		public function has_access(array $args): bool {
+			if (!isset($_GET['token']))
+				return false;
+			if ($_GET['token'] === "android_mind_key_2")
+				return true;
+			if ($_GET['token'] === "site_mind_key_3")
+				return true;
+
+			return false;
+		}
+
 		public function get_html(array $args): string {
 			$obj = $this->handle($args);
 			header('Content-Type: application/json');
