29b5230364c642008df5cc2371a1e5a817cfe3f5
Semenov Roman <3908441@gmail.com>
2018-11-13 11:06:00 +0300

    Db methods class

---
+++ src/Db.php
@@ -0,0 +1,21 @@
+<?php
+	namespace Mind;
+
+	require_once __DIR__."/../config.php";
+
+	class Db {
+		public static function query(...$args) {
+			$sql = new \SafeMySQL([
+				'user' => DB_USER,
+				'db' => DB_NAME,
+				'pass' => DB_PASSWORD
+			]);
+
+			return $sql->query(...$args);
+		}
+
+		public static function query_assoc(...$args) {
+			return static::query(...$args)->fetch_assoc();
+		}
+	}
+?>
