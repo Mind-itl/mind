48c3e51887a9fdaf30e91bb98b53bfb4f638be01
Semenov Roman <3908441@gmail.com>
2018-12-07 19:18:21 +0300

    add tests and fixes

--- tests/acceptance/PointsCest.php
+++ tests/acceptance/PointsCest.php
@@ -1,12 +1,17 @@
 <?php
 
 class PointsCest {
-	public function _before(AcceptanceTester $I) {
+	public function myPointsWorks(AcceptanceTester $I) {
 		$I->loginAsStudent();
 		$I->click("Мои баллы");
+		$I->seeInTitle("Баллы");
+		$I->see(TEST_STUDENT_NAME);
+		$I->see("Баланс");
 	}
 
-	public function pageWorks(AcceptanceTester $I) {
+	public function customStudentPointsWork(AcceptanceTester $I) {
+		$I->loginAsStudent();
+		$I->amOnPage("/points/". TEST_STUDENT_LOGIN);
 		$I->seeInTitle("Баллы");
 		$I->see(TEST_STUDENT_NAME);
 		$I->see("Баланс");
