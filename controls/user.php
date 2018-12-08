24947c8263804d9766ebdcf14328d02b097f1c74
Semenov Roman <3908441@gmail.com>
2018-12-08 09:20:50 +0300

    user fixes

--- controls/user.php
+++ controls/user.php
@@ -2,7 +2,7 @@
 	namespace Mind\Controls;
 
 	use Mind\Db\Users;
-	use Mind\Server\{Control, Utils};
+	use Mind\Server\{Control, Utils, Route};
 
 	class User extends Control {
 		public function has_access(array $args): bool {
@@ -10,10 +10,14 @@
 		}
 
 		protected function get_data(array $args): array {
-			if (isset($args[1]) && $args[1]!="")
-				$user = Users::get($args[1], true);
-			else 
-				$user = Utils::get_curr();
+			if (isset($args[1]) && $args[1]!="") {
+				if (Users::has_login($args[1], true))	
+					$user = Users::get($args[1], true);
+				else
+					Route::not_found();
+			}
+
+			$user = $user ?? Utils::get_curr();
 
 			return [
 				"he" => $user
