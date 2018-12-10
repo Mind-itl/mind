81d20e9d5eec662b879e67a16d6bcc86a4f7c6a6
Semenov Roman <3908441@gmail.com>
2018-12-10 09:19:58 +0300

    add roles' names

--- src/Users/Role.php
+++ src/Users/Role.php
@@ -11,7 +11,6 @@ class Role {
 	const PREDMET  = "predmet";
 	const ZAM      = "zam";
 	const CLASSRUK = "classruk";
-	const TEACHER  = "teacher";
 	const PEDORG   = "pedorg";
 	const SOCPED   = "socped";
 	const MEDIC    = "medic";
@@ -21,4 +20,22 @@ class Role {
 	const SECRETAR  = "secretar";
 	const DEPUTAT   = "deputat";
 	const STAROSTA  = "starosta";
+
+	public static function get_role_name(string $role) {
+		return [
+			static::TEACHER  => "учитель",
+			static::DIRIC    => "директор",
+			static::PREDMET  => "предметник",
+			static::ZAM      => "заместитель директора",
+			static::CLASSRUK => "классный руководитель",
+			static::SOCPED   => "социальный педагог",
+			static::MEDIC    => "медик",
+
+			static::STUDENT   => "ученик",
+			static::PRESIDENT => "президент совета лицеистов",
+			static::SECRETAR  => "секретарь совета лицеистов",
+			static::DEPUTAT   => "депутат совета лицеистов",
+			static::STAROSTA  => "староста"
+		][$role];
+	}
 }
