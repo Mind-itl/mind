dd42267cb69ddc1cfbab5e3512e54022f85e1ed6
Semenov Roman <3908441@gmail.com>
2018-12-10 09:14:30 +0300

    add list of roles

---
+++ src/Users/Role.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Mind\Users;
+
+use Mind\Db\{Users, Causes, Db, Transactions};
+
+class Role {
+	const TEACHER  = "teacher";
+	const DIRIC    = "diric";
+	const PREDMET  = "predmet";
+	const ZAM      = "zam";
+	const CLASSRUK = "classruk";
+	const TEACHER  = "teacher";
+	const PEDORG   = "pedorg";
+	const SOCPED   = "socped";
+	const MEDIC    = "medic";
+
+	const STUDENT   = "student";
+	const PRESIDENT = "president";
+	const SECRETAR  = "secretar";
+	const DEPUTAT   = "deputat";
+	const STAROSTA  = "starosta";
+}
