088b7947a5e076e1b3c075e92582ece3454ce317
Semenov Roman <3908441@gmail.com>
2018-12-11 13:02:58 +0300

    add answering ability

--- controls/ask.php
+++ controls/ask.php
@@ -1,7 +1,7 @@
 <?php
 	namespace Mind\Controls;
 
-	use Mind\Db\{Db, Users, Transactions};
+	use Mind\Db\{Db, Users, Transactions, Notifications};
 	use Mind\Server\{Control, Utils};
 	use Mind\Users\{User, Teacher, Student};
 
@@ -13,28 +13,14 @@
 		}
 
 		protected function get_data(array $args): array {
-			if (Utils::isset_post_fields("question")) {
-				$question = $_POST["question"];
+			if (Utils::isset_post_fields("question"))
+				$this->add_question();
 
-				Db::query("
-					INSERT INTO questions (
-						LOGIN, QUESTION
-					) VALUES (
-						?s, ?s
-					)", Utils::get_curr()->get_login(), $question
-				);
+			if (Utils::isset_post_fields("remove_id"))
+				$this->remove_question();
 
-				$result = "success";
-			}
-
-			if (Utils::isset_post_fields("remove_id")) {
-				$id = intval($_POST["remove_id"]);
-				Db::query("
-					DELETE FROM questions
-					WHERE ID = ?i
-					", $id
-				);
-			}
+			if (Utils::isset_post_fields("answer", "id"))
+				$this->answer_question();
 
 			$r = Db::query("
 				SELECT * FROM questions
@@ -49,7 +35,7 @@
 					"time" => new \DateTime($q["TIME"]),
 					"text" => $q["QUESTION"],
 					"from" => Users::get($q["LOGIN"]),
-					"answered" => isset($q["ANSWERER"]),
+					"answered" => isset($q["ANSWERER_LOGIN"]),
 					"id" => $q["ID"]
 				];
 
@@ -75,6 +61,70 @@
 				"can_remove" => $can_remove,
 				"can_answer" => $can_answer
 			];
+		}
+
+		private function add_question() {
+			$question = $_POST["question"];
+
+			Db::query("
+				INSERT INTO questions (
+					LOGIN, QUESTION
+				) VALUES (
+					?s, ?s
+				)", Utils::get_curr()->get_login(), $question
+			);
+
+			$result = "success";
+		}
+
+		private function remove_question() {
+			$id = intval($_POST["remove_id"]);
+
+			Db::query("
+				DELETE FROM questions
+				WHERE ID = ?i
+				", $id
+			);
+		}
+
+		private function answer_question() {
+			$answer = $_POST["answer"];
+			$answer = trim($answer);
+
+			$id = intval($_POST['id']);
+
+			if ($answer === "") {
+				Db::query("
+					UPDATE questions
+					SET
+						ANSWER = NULL,
+						ANSWERER_LOGIN = NULL
+					WHERE ID = ?i
+					", $id
+				);				
+
+				return;
+			}
+
+			Db::query("
+				UPDATE questions
+				SET
+					ANSWER = ?s,
+					ANSWERER_LOGIN = ?s
+				WHERE ID = ?i
+				", $answer, Utils::get_curr()->get_login(), $id
+			);
+
+			$from_user = Users::get(
+				Db::query_assoc("
+					SELECT LOGIN
+					FROM questions
+					WHERE ID = ?i
+					", $id
+				)["LOGIN"]
+			);
+
+			Notifications::add($from_user, Utils::get_curr(), "На ваш вопрос ответили");
 		}
 	}
 ?>
