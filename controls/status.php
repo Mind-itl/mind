d62b7d1010e14691eeffdff7df357ca686ce6903
Semenov Roman <roman.semenov@protonmail.com>
2018-12-13 15:28:17 +0300

    add security to status page

--- controls/status.php
+++ controls/status.php
@@ -3,6 +3,7 @@
 
 	use Mind\Server\{Control, Utils};
 	use Mind\Db\Statuses;
+	use Mind\Users\{User, Teacher, Student, Role};
 
 	class Status extends Control {
 		public function has_access(array $args): bool {
@@ -10,11 +11,26 @@
 		}
 
 		public function get_data(array $args): array {
+			$groups = Statuses::get_students_by_classes();
 
+			foreach ($groups as &$group) {
+				$group["can_edit"] =
+					Utils::get_curr()->has_role(Role::ZAM) ||
+					Utils::get_curr()->has_role(Role::SOCPED) ||
+					Utils::get_curr()->has_role(Role::DIRIC) ||
+					(
+						Utils::get_curr()->has_role(Role::CLASSRUK) &&
+						Utils::get_curr()->get_role_arg(Role::CLASSRUK) == $group["name"]
+					) ||
+					(
+						Utils::get_curr() instanceof Student &&
+						Utils::get_curr()->get_group_name() == $group["name"]
+					);
+			}
 
 			return [
 				"statuses" => Statuses::get_types(),
-				"groups" => Statuses::get_students_by_classes()
+				"groups" => $groups
 			];
 		}
 	}
